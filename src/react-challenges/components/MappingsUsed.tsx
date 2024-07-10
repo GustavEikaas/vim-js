21b3225273c32f8290b782cf2cefb6f00ad58594
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-07-10 18:09:32 +0200

    small refactor

--- src/react-challenges/components/MappingsUsed.tsx
+++ src/react-challenges/components/MappingsUsed.tsx
@@ -3,6 +3,9 @@ type MappingsUsedProps = {
   least: number;
 }
 export const MappingsUsed = ({ spent, least }: MappingsUsedProps) => {
-  const textColorClass = spent > least ? "text-red" : "text-black";
-  return (<div>Mappings used: <span className={textColorClass}>{spent}</span>/{least}</div>)
+  return (
+    <div>Mappings used: 
+      <span className={`${spent > least ? "text-red" : "text-black"}`}> {spent}</span>/{least}
+    </div>
+  )
 }
